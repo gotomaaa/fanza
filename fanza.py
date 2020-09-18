8e9e143033df4b5a084f81f1182cc9d1368753f9
gotomaaa <64519574+gotomaaa@users.noreply.github.com>
2020-09-18 20:57:06 +0900

    Update fanza.py

--- fanza.py
+++ fanza.py
@@ -14,4 +14,4 @@ api = dmm.API(api_id=api_id, affiliate_id=affiliate_id)
 
 def getFanzaItem(arg):
     item_search = api.item_search(site="FANZA", hits=3, keyword=arg)
-    return item_search
+    return iccccscstem_search
